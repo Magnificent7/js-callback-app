c64bebeba3bbb31fd5a3b443eae375038c9d52fe
dani <dani.zaghian@gmail.com>
2017-06-18 11:20:21 -0700

    added dom manipulation with colors

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -18,13 +18,24 @@ function getData() {
     var htmlString = '';
     for (var i = 0; i < response.length; i++) {
       var foodtruck = response[i];
+      htmlString += '<div onclick="changeFoodtruckColor(this)">';
       htmlString += '<h4>' + foodtruck.applicant + '</h4>';
       htmlString += '<p>' + foodtruck.optionaltext + '</p>';
       htmlString += '<p>' + foodtruck.dayofweekstr + '</p>';
       htmlString += '<p>' + foodtruck.starttime + '</p>';
+      htmlString += '</div>';
     }
     foodtrucksDiv.innerHTML = htmlString;
   });
+}
+
+function changeFoodtruckColor(foodtruckDiv) {
+  foodtruckDiv.classList.toggle('blue-text');
+  // if (foodtruckDiv.style.color == 'blue') {
+  //   foodtruckDiv.style.color = '';
+  // } else {
+  //   foodtruckDiv.style.color = "blue";
+  // }
 }
 
 
